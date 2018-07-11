6152177cd62fbadb8be82541a451926ea344b3fb
keyama4 <yhr09749@gmail.com>
2018-07-11 16:14:30 +0900

    Add load module

---
+++ src/redux/modules/load.js
@@ -0,0 +1,55 @@
+import { put, takeEvery } from 'redux-saga/effects';
+
+/**
+ * actions
+ */
+const START_LOAD = 'START_LOAD';
+const COMPLETE_LOAD = 'COMPLETE_LOAD';
+
+/**
+ * reducers
+ */
+const initialState = {
+  loading: false,
+};
+
+export const reducers = (state = initialState, action) => {
+  switch (action.type) {
+    case START_LOAD:
+    const COMPLETE_LOAD = 'COMPLETE_LOAD';
+      return {
+        ...state,
+        ...action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+/**
+ * action creaters
+ */
+export const startLoad = () => {
+  return {
+    type: START_LOAD,
+    payload: { loading: true }
+  };
+}
+
+const completeStartLoad = () => {
+  return {
+    type: START_LOAD,
+    payload: { loading: false }
+  };
+}
+
+/**
+ * sagas
+ */
+export function* startLoadSaga() {
+  yield put(completeStartLoad());
+}
+
+export function* saga() {
+  yield takeEvery(START_LOAD, startLoadSaga);
+}
