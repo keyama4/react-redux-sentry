38bf930894d81e7db2792d88f563c6ce0dfc15ee
keyama4 <yhr09749@gmail.com>
2018-07-11 14:08:55 +0900

    Fix index js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './app';
+import ErrorBoundary from './error-boundary';
 
 // TODO: change config arguments to your dsn
 window.Raven.config("<YOUR_DSN>").install();
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<ErrorBoundary><App /></ErrorBoundary>, document.getElementById('root'));
