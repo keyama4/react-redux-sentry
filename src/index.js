53aa7cb031bf44c6ebd77c1832efec1f4dc11f8e
keyama4 <yhr09749@gmail.com>
2018-07-11 13:46:34 +0900

    Add raven config to index js

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// TODO: change config arguments to your dsn
+window.Raven.config("<YOUR_DSN>").install();
 ReactDOM.render(<App />, document.getElementById('root'));
