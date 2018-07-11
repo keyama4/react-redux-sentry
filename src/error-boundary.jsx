c553d492fff3bf6c62ef498814d76f4edd44dc8f
keyama4 <yhr09749@gmail.com>
2018-07-11 14:06:01 +0900

    Add error boundary

---
+++ src/error-boundary.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+export default class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  componentDidCatch(error, info) {
+    this.setState({ hasError: true });
+    window.Raven.captureException(error, { extra: info });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Something went wrong.</h1>;
+    }
+    return this.props.children;
+  }
+}
