2a7bc1105d7ace932067c0689294db42cb7a09a5
keyama4 <yhr09749@gmail.com>
2018-07-11 18:21:23 +0900

    Fix store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,18 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import logger from 'redux-logger';
-
+import createRavenMiddleware from "raven-for-redux";
 import reducers from './reducers';
 import saga from './saga';
 
-const sagaMiddleware = createSagaMiddleware();
-const middleware = [sagaMiddleware];
+const sagaMiddleware = createSagaMiddleware({
+  onError(error) {
+    setImmediate(() => {
+      throw error;
+    });
+  }
+});
+const middleware = [sagaMiddleware, createRavenMiddleware(window.Raven)];
 
 if (process.env.NODE_ENV !== 'production') {
   middleware.push(logger);
